673f2036dca5890d3194b049f3fd8f76f5db2991
Àngel Buigues Andres <angelbuiguesandres.teulada@gmail.com>
2022-06-14 12:38:00 +0200

    checkpoint3

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 
 import csv
 def read_data(fichero1, fichero2):
-    
+    diccionario={}
+
     with open(fichero1, 'r') as file:
         reader = csv.reader(file)
-        diccionario={}
         for row in reader:
-            diccionario[row[0]]=row[2]
-            print(row)
-
+              with open(fichero2, 'r') as file2:
+                reader2 = csv.reader(file2)
+                for row2 in reader2:
+                    if(row[0]==row2[0]):
+                        diccionario[row[0]]={
+                            'description':row2[3],
+                            'id':row2[1],
+                            'lat':row[4],
+                            'lon':row[5],
+                            'name':row2[2]}
 
     print("---Diccionario----")
-
     print(diccionario)
-    '''
-     with open(fichero2, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
+    return diccionario
 
+def get_name_description(clave, diccionario):
+    try:
+
+        print("Name: ")
+        print(diccionario[clave]['name'])
+        print("Description: ")
+        print(diccionario[clave]['description'])
+
+    except IndexError as err:
+         print("se ha producido una excepción: "+str(err))
     
-    '''
+    
+    
    
 
 if __name__ == "__main__":
     fichero1 = "stops_data.csv"
     fichero2 = "stops.csv"
 
-    read_data(fichero1,fichero2)
+    diccionario = read_data(fichero1,fichero2)
+    get_name_description('1080', diccionario)
+
+
+
+
+
 
 
